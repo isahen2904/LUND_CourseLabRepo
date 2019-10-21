ce326930fc27650b75bbe540ef85bba84f18a7b6
DeltaMod <33879770+DeltaMod@users.noreply.github.com>
2019-10-22 00:59:17 +0200

    fixed some problems, still can't fix life

--- L3-Pol_Analysis.py
+++ L3-Pol_Analysis.py
@@ -26,17 +26,17 @@ for Name in range(len(DList)):
 
     Dat.append(np.array([(float(elemlist[n][1]),float(elemlist[n][2])) for n in range(len(elemlist))]) )
 #Index 4->13 is crystal rotations 0:5:45
-d     = 10**-3*np.array([0.24, 1.00, 3.00, 9.02])
+d     = 10**-3*np.array([0.24, 1.00, 3.00, 9.02]) #Originally in mm => 10^3 microns
 
 Dat2    = [signal.savgol_filter(Dat[n][:,1], 51, 3) for n in range(len(Dat))] #We smooth out all signals, since they were pretty rough
 PLoc    = [signal.find_peaks(abs(Dat2[n]-max(Dat2[n])),width=20) for n in range(len(Dat))]      #We use a peak finder to determine the indices of all valleys
-PLoc[3] = signal.find_peaks(Dat[3][:,1],width=8)    #We use a peak finder to determine the indices of all valleys
+PLoc[3] = signal.find_peaks(Dat[3][:,1],width=9,prominence=50)    #We use a peak finder to determine the indices of all valleys
 
-NSel   = [10,15,20]
+NSel   = [5,13,21]
 NMod   = [NSel[n] - NSel[0] for n in range(len(NSel))]
 WRange = Dat[3][:,0]*10**-9
 biflam = [Dat[3][PLoc[3][0][NSel[n]],0]*10**-9 for n in range(len(NSel))]
-a = 2
+a = 3
 D1 = 2*d[a]*(1/(biflam[0]**2 - 1/(biflam[1]**2))); D2 = 2*d[a]*(1/(biflam[2]**2 - 1/(biflam[1]**2)))
 
 
@@ -58,23 +58,29 @@ Test1 = (2*d[a]*(Aeq+Beq/(biflam[1]**2))/(biflam[1]) - 1 + 2*NMod[1])/2
 Test2 = (2*d[a]*(Aeq+Beq/(biflam[2]**2))/(biflam[2]) - 1 + 2*NMod[2])/2
 
 #%% Alternate solution using np linalg:
-MA = np.array([[1,1/biflam[0]**2,-biflam[0]/d[a]], [1,1/biflam[1]**2,-biflam[1]/d[a]], [1,1/biflam[2]**2,-biflam[2]/d[a]]])
-MB = np.array([biflam[0]/(2*d[a]),biflam[1]/(2*d[a])-(NMod[1]*biflam[1])/(d[a]),biflam[2]/(2*d[a])-(NMod[2]*biflam[2])/(d[a])])
+MA = np.array([[1,1/(biflam[0]**2),-biflam[0]/d[a]], 
+               [1,1/(biflam[1]**2),-biflam[1]/d[a]], 
+               [1,1/(biflam[2]**2),-biflam[2]/d[a]]])
+MB = np.array([[biflam[0]/(2*d[a])],
+               [biflam[1]/(2*d[a])-(NMod[1]*biflam[1])/(d[a])],
+               [biflam[2]/(2*d[a])-(NMod[2]*biflam[2])/(d[a])]])
 ABXSolve = np.linalg.solve(MA, MB)
 print(ABXSolve)
-print(d[a])
+
 #%% Birefringeance Stuff
 Deltn = []
 ABTest = True
 
-for i in range(len(PLoc[3][0])):
+for i in range(0,len(PLoc[3][0])):
     if ABTest == False:
-        Deltn.append([(2*(ABXSolve[2]-i+NSel[0])+1)*(PLoc[3][0][i]*10**-9)/(2*d[a])])
+        Deltn.append((2*(ABXSolve[2]-i))*(Dat[3][PLoc[3][0][i],0]*10**-9)/(2*d[a]))
     if ABTest == True:
-        Deltn.append(ABXSolve[0]+ABXSolve[1]/(PLoc[3][0][i]*10**-9)**2)
+        Deltn.append(ABXSolve[0]+ABXSolve[1]/(Dat[3][PLoc[3][0][i],0]*10**-9)**2)
+DELTN = [Deltn[0]-Deltn[i] for i in range(1,len(Deltn))]
 fig0 = plt.figure(0)
 ax = fig0.gca()
-ax.plot(PLoc[3][0],Deltn)
+ax.plot(Dat[3][PLoc[3][0],0]*10**-9,Deltn)
+#ax.plot(PLoc[3][0],DELTN)
 plt.xlabel('Wavelength [nm]')
 plt.ylabel('Birefringeance')
 plt.grid()
@@ -84,12 +90,12 @@ fig1.clf()
 ax = fig1.gca()
 for m in range(3,4): 
     m = 3
-    ax.plot(Dat[m][:,0],Dat[m][:,1]) 
-    ax.scatter(Dat[m][PLoc[m][0],0],Dat[m][PLoc[m][0],1])
+    ax.plot(Dat[m][:,0]*10**-9,Dat[m][:,1]) 
+    ax.scatter(Dat[m][PLoc[m][0],0]*10**-9,Dat[m][PLoc[m][0],1])
     ax.scatter(biflam,Dat[m][PLoc[m][0][NSel],1])
 plt.grid()
 plt.autoscale(enable=True,tight=True)
-plt.xlabel('Wavelength [nm]')
+plt.xlabel('Wavelength [m]')
 plt.ylabel('Intensity [a.u]')
 #%% Optical Activity Stuff:
 
@@ -102,7 +108,7 @@ for n in range(4,14):
     
 for n in range(4,14):
     ax.scatter(Dat[n][PLoc[n][0],0],Dat[n][PLoc[n][0],1])
-plt.xlabel('Wavelength [nm]')
+plt.xlabel('Wavelength [m]')
 plt.ylabel('Intensity [a.u]')
 beta  = np.array([0, 5, 10, 15, 20, 25, 30, 35, 40, 45])
 lambd = [10**-9*Dat[n][PLoc[n][0][-1],0] for n in range(4,14)]
